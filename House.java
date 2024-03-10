51eb0266e40f2f8a465d028405b1dd42459c8ef3
Mack Case <hcase@smith.edu>
2024-03-10 16:56:10 -0400

    house class has necessary attributes, accessors, and methods

--- House.java
+++ House.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 public class House extends Building {
 
   boolean hasElevator;
-  boolean hasDiningRoom;
-  ArrayList<String> residents;
+  private boolean hasDiningRoom;
+  private ArrayList<String> residents;
 
   public House(String name, String address, int nFloors, 
               boolean hasElevator, boolean hasDiningRoom) { //don't need to pass in an empty arrayList
@@ -18,10 +18,51 @@ public class House extends Building {
     System.out.println("You have built a house: 🏠");
   }
 
+  //accessor for dining room
+  public boolean getHasDiningRoom(){
+    return this.hasDiningRoom;
+  }
+
+  //accessor for number of residents
+  public int getNResidents(){ 
+    return residents.size();
+  }
+
+  //check to see if they live here
+  public boolean isResident(String person){
+    if (residents.contains(person)){
+      return true; //add name + "is a resident"?
+    }
+    return false; //add name + "is not a resident"?
+  }
+
+  //move someone in
+  public void moveIn(String name){
+    if (!residents.contains(name)){
+      residents.add(name);
+    }
+    //throw exception
+  }
+
+  public String moveOut(String name){
+    if (residents.contains(name)){
+      residents.remove(name);
+      return name + " has moved out.";
+    }
+    return name + "does not live here";
+  }
   public static void main(String[] args) {
     House myHouse = new House("Ninomiya", "69 Paradise Rd", 2, false, false);
     System.out.println(myHouse); // toString method comes from Building - otherwise it would return memory location 
+    System.out.println(myHouse.getHasDiningRoom());
+    System.out.println(myHouse.getNResidents());
+    myHouse.moveIn("Pria");
+    System.out.println(myHouse.isResident("Pria"));
+    System.out.println(myHouse.moveOut("Pria"));
+    System.out.println(myHouse.isResident("Pria"));
+    
   }
+
 
 //   public String toString() { // can override inhereted stuff by rewriting
 //     String desc = super.toString(); 
